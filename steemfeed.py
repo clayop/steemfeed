caa5bfcafb0e425fb91da1b8d228434f323f64ed
Jaewoo Cho <clayop@gmail.com>
2016-04-28 09:03:57 -0700

    Added more features
    (1) Left time to update log
    (2) Minimum change/Maximum feed age field
    (3) Initial feed publish
    (4) Randomness in interval
    (5) Manual confirmation using console or Telegram

--- steemfeed.py
+++ steemfeed.py
@@ -1,16 +1,31 @@
 import time
 import dateutil.parser
 import requests
+import random
+import telegram
 from steemapi import SteemWalletRPC
 
 # Config
-interval = 3600       # Feed publishing interval in seconds
-freq = 60             # Frequency of parsing trade histories
-null_price = 0.500    # Current Price (Just in case)
-witness = "clayop"    # Your witness name
+interval_init  = 60*60*2             # Feed publishing interval in seconds
+rand_level     = 0.10                # Degree of randomness of interval
+freq           = 60                  # Frequency of parsing trade histories
+min_change     = 0.03                # Minimum price change to publish feed
+max_age        = 60*60*24            # Maximum age of price feed
+manual_conf    = 0.30                # Maximum price change without manual confirmation
+use_telegram   = 0                   # If 1, you can confirm manual price feed through Telegram
+telegram_token = "telegram_tocken"   # Create your Telegram bot at @BotFather (https://telegram.me/botfather)
+telegram_id    = 0                   # Get your telegram id at @MyTelegramID_bot (https://telegram.me/mytelegramid_bot)
+rpc_host       = "localhost"
+rpc_port       = 8092
+witness        = "yourwitness"       # Your witness name
 
-print("Connecting to STEEM RPC")
-rpc = SteemWalletRPC("localhost", 8092, "", "")
+
+print("Connecting to Steem RPC")
+rpc = SteemWalletRPC(rpc_host, rpc_port, "", "")
+print("Connected")
+print("Connecting to Telegram")
+bot = telegram.Bot(token=telegram_token)
+print("Connected")
 
 def btc_usd():
     n = 0
@@ -37,18 +52,76 @@ def btc_usd():
     res = (bitfinex + coinbase + okcoin + bitstamp)/n
     return res
 
+def rand_interval(intv):
+    intv += intv*rand_level*random.uniform(-1, 1)
+    if intv < 60*60:
+        intv = 60*60
+    elif intv > 60*60*24*7:
+        intv = 60*60*24*7
+    return(int(intv))
+
+def confirm(pct, p):
+    if use_telegram == 0:
+        conf = input("Your price feed change is over " + format(pct*100, ".1f") + "% (" + p + "USD/STEEM) If you confirm this, type 'confirm': ")
+        if conf.lower() == "confirm":
+            return True
+        else:
+            reconf = input("You denied to publish this feed. Are you sure? (Y/n): ")
+            if reconf.lower() == "n":
+                conf = input("If you confirm this, type 'confirm': ")
+                if conf.lower() == "confirm":
+                    return True
+                else:
+                    print("Publishing denied")
+                    return False
+            else:
+                print("Publishing denied")
+                return False
+    elif use_telegram == 1:
+        last_update_id = bot.getUpdates()[-1].update_id
+        custom_keyboard = [["deny"]]
+        reply_markup = {"keyboard":custom_keyboard, "resize_keyboard": True}
+        conf_msg = ("Your price feed change is over " + format(pct*100, ".1f") + "% (" + p + "USD/STEEM) If you confirm this, type 'confirm'")
+        bot.sendMessage(chat_id=telegram_id, text=conf_msg, reply_markup=reply_markup)
+        while True:
+            updates = bot.getUpdates(offset=last_update_id, limit = 30)[-1]
+            chat_id = updates.message.chat_id
+            update_id = updates.update_id
+            cmd = updates.message.text
+            if update_id > last_update_id:
+                if chat_id == telegram_id and cmd.split()[0] == "confirm":
+                    bot.sendMessage(chat_id=telegram_id, text="Publishing confirmed")
+                    return True
+                elif chat_id == telegram_id and cmd.split()[0] == "deny":
+                    bot.sendMessage(chat_id=telegram_id, text="Publishing denied")
+                    return False
+                else:
+                    bot.sendMessage(chat_id=telegram_id, text="Please type 'confirm' or push 'deny' button", reply_markup=reply_markup)
+            time.sleep(3)
+
 
 if __name__ == '__main__':
     steem_q = 0
     btc_q = 0
+    last_update_t = 0
+    interval = rand_interval(interval_init)
     try:
-        price = (requests.get("https://bittrex.com/api/v1.1/public/getticker?market=BTC-STEEM").json()["result"]["Last"])*btc_usd()
+        last_price = (requests.get("https://bittrex.com/api/v1.1/public/getticker?market=BTC-STEEM").json()["result"]["Last"])*btc_usd()
+        print("The current market price is " + format(last_price, ".3f") + " USD/STEEM")
     except:
-        price = null_price
+        last_price = float(rpc.get_feed_history()["current_median_history"]["base"].split()[0])/float(rpc.get_feed_history()["current_median_history"]["quote"].split()[0])
+        print("Failed to fetch market price. Current median feed price " + format(last_price, ".3f") + " USD/STEEM will be used")
+
     start_t = (time.time()//freq)*freq - freq
     last_t = start_t - 1
-    print("Please wait until the end of first interval: " + str(interval) + " seconds")
-    print("Don't forget to unlock your cli_wallet")
+    print("Please be advised that your first price feed will be published when the price changes over " + format(min_change*100, ".1f") + "% or after " + format(max_age/3600, ".0f") + " hours")
+    init_pub = input("Will you publish this price feed (" + format(last_price, ".3f") + " USD/STEEM)? (y/N) ")
+    if init_pub.lower() == "y":
+        rpc.publish_feed(witness, {"base": format(last_price, ".3f") +" SBD", "quote":"1.000 STEEM"}, True)
+        print("Published price feed: " + format(last_price, ".3f") + " USD/STEEM at " + time.ctime())
+    else:
+        last_price = 0.0001
+        print("Please confirm your first feed price after " + str(int(interval/60)) + " minutes")
 
     while True:
         curr_t = (time.time()//freq)*freq - freq
@@ -70,12 +143,27 @@ if __name__ == '__main__':
                 pass
         if curr_t - start_t >= interval:
             if steem_q > 0:
-                price = format(btc_q/steem_q*btc_usd(), ".3f")
-                rpc.publish_feed(witness, {"base": price +" SBD", "quote":"1.000 STEEM"}, True)
-                print("Published price feed: " + price + " USD/STEEM at " + time.ctime())
-                steem_q = 0
-                btc_q = 0
+                price = btc_q/steem_q*btc_usd()
+                price_str = format(price, ".3f")
+                if (abs(1 - price/last_price) < min_change) and (curr_t - last_update_t < max_age):
+                    print("No significant price change or the feed is not obsolete")
+                    print("Last price: " + format(last_price, ".3f") + "  Current price: " + price_str + "  " + format((price/last_price*100 - 100), ".1f") + "%  / Feed age: " + str(int((curr_t - last_update_t)/3600)) + " hours")
+                else:
+                    if abs(1 - price/last_price) > manual_conf:
+                        if confirm_telegram(manual_conf, price_str) is True:
+                            rpc.publish_feed(witness, {"base": price_str +" SBD", "quote":"1.000 STEEM"}, True)
+                            print("Published price feed: " + price_str + " USD/STEEM at " + time.ctime())
+                    else:
+                        rpc.publish_feed(witness, {"base": price_str +" SBD", "quote":"1.000 STEEM"}, True)
+                        print("Published price feed: " + price_str + " USD/STEEM at " + time.ctime())
+                    steem_q = 0
+                    btc_q = 0
+                    last_price = price
+                    last_update = curr_t
             else:
                 print("No trades occured during this period")
+            interval = rand_interval(interval_init)
             start_t = curr_t
+        left_min = (interval - (curr_t - start_t))/60
+        print(str(int(left_min)) + " minutes to next update   \r", end="")
         time.sleep(freq*0.7)
